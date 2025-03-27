4a35bb6b0dc0a2b2ea35cc208326c0cef4de7d82
Vishal-475 <cool.vichuvishal4@gmail.com>
2025-03-27 17:48:34 +0530

    Added recipe categories migration, model, and basic seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,9 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            'name' => 'John Doe',  // Replace with any name
+            'email' => 'john.doe@example.com', // Replace with any email
+            'password' => Hash::make('SecurePass123!'), // A more secure password
         ]);
     }
 }
